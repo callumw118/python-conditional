ddbd6f751682133768f644a7656dbed8cc628000
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-13 18:26:46 +0100

    Adds while loop

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,9 +1,13 @@
-guess = input("Guess the number between 0-10 ")
+solved = False
 
+while solved == False:
+    guess = input("Guess the number between 0-10: ")
 
-if int(guess) == 4:
-    print("You have guessed the correct number")
-elif int(guess) == 3 or int(guess) == 5:
-    print("So close, you were one number off")
-else:
-    print("Sorry wrong number")
+    if int(guess) == 4:
+        print("You have guessed the correct number")
+        solved = True
+    elif int(guess) == 3 or int(guess) == 5:
+        print("So close, you were one number off")
+
+    else:
+        print("Sorry wrong number")
