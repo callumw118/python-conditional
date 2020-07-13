a5d04235f72b77fdbb4ae1eae730c69023d9692b
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-13 18:00:42 +0100

    initial commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+guess = input("Guess the number between 0-10 ")
+
+
+if int(guess) == 4:
+    print("You have guessed the correct number")
+elif int(guess) == 3 or int(guess) == 5:
+    print("So close, you were one number off")
+else:
+    print("Sorry wrong number")
